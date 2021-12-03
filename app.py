015af5267ca7b6835c66427efaf1cf7244029947
dkletter <dan@kletter.us>
2021-12-02 21:32:26 -0800

    Working on importing json into mongodb

--- app.py
+++ app.py
@@ -1,50 +1,56 @@
+import os
+import sqlite3
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from config import username
-from config import passwd
+
+# Database setup
+path = "data/portland.sqlite"
+engine = create_engine(f"sqlite:///{path}")
+
+# Reflect existing database ito a new model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to tables
+Restaurants = Base.classes.restaurants
 
 app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://<username>:<passwd>@localhost:5432/restaurant_db"
-db = SQLAlchemy(app)
-
-class Biz(db.Model):
-    __tablename__="yelp_data"
-    id=db.Column(db.Serial, primary_key=True)
-    name=db.Column(db.Text)
-	address=db.Column(db.Text)
-	city=db.Column(db.Text)
-	state=db.Column(db.Text)
-	postal_code=db.Column(db.Text)
-	latitude=db.Column(db.Decimal)
-	longitude=db.Column(db.Decimal)
-	stars=db.Column(db.Decimal)
-	review_count=db.Column(db.Integer)
-	categories=db.Column(db.Text)
-
-    def __init__(self, name, address, city, state, postal_code, latitude, longitude, stars, review_count, categories):
-        self.name=name
-        self.address=address
-        self.city=city
-        self.state=state
-        self.postal_code=postal_code
-        self.latitude=latitude
-        self.longitude=longitude
-        self.stars=stars
-        self.review_count=review_count
-        self.categories=categories
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/submit", methods=["GET", "POST"])
-    if request.method == "GET":
-        results = db.session.query(Biz).filter(Biz.id == 1)
-        for result in results:
-            print(result.name)
 
-    return render_template("success.html", data=name)
+@app.route("/", methods = ["GET"])
+def search():
+    try:
+        if request.method == "GET":
+            session = Session(engine)
+            results = session.query(Restaurants.name, Restaurants.categories).all()
+            session.close()
+            restaurant_list = []
+            for name, categories in results:
+                restaurant_dict = {}
+                restaurant_dict['name'] = name
+                restaurant_dict['categories'] = categories
+
+
+
+            name = request.args.get("name")
+            conn = sqlite3.connect(path)
+            cursor = conn.cursor()
+            query = "SELECT * from restaurants WHERE categories = vietnamese"
+            result = cursor.execute(query)
+            result = result.fetchall()[0][0]
+            return render_template("index.html", restaurants = result)
+    except:
+        return render_template("index.html", restaurants = "")
 
 if __name__ == "__main__":
     app.run(debug=True)
